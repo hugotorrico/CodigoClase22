267eb2b62d757174d17031fc5c78cf9d8b113a14
Hugo Torrico <torricomarquezh@gmail.com>
2024-05-22 22:22:05 -0500

    IEnumerable vs List

---
+++ LINQ/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Persona
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Ciudad { get; set; }
+    }
+
+    public static class PersonaData
+    {
+    
+        public static List<Persona> GetPersonas()
+        {
+            var result= new List<Persona>();
+            for (int i = 0; i < 2000000; i++)
+            {
+                result.Add(new Persona { Apellidos = "Apellidos "+ i, Nombres = "Nombres " + i, Ciudad = "Ciudad " + i });
+            }
+         return result;
+        }
+    
+    
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,146 @@
+﻿
+//Obtener los números pares 
+using LINQ;
+using System.ComponentModel.DataAnnotations;
+
+void MostrarNumerosPares()
+{
+    var numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    var numerosPares = new List<int>();
+    foreach (var item in numeros)
+    {
+        if (item % 2 == 0)
+        {
+            numerosPares.Add(item);
+        }
+    }
+
+    foreach (var item in numerosPares)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+void MostrarNumerosParesLINQ()
+{
+    var numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    
+    //LINQ
+    var numerosPares= (from c in numeros 
+                       where c%2==0 
+                       select c).ToList(); 
+
+
+    foreach (var item in numerosPares)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+void MostrarNumerosParesLambda() //=>Anónimas
+{
+    var numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+   
+    var numerosPares = numeros.Where(x=>x % 2 == 0 ).ToList();
+
+    var query = numeros.Where(x => x % 2 == 0);
+
+    foreach (var item in numerosPares)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+
+void IEnumerableVsList() //=>Anónimas
+{
+    //Pares Mayores a 5
+
+    var numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };//10 elementos (new)
+    
+    //Primer escenario
+    var numerosPares = numeros.Where(x => x % 2 == 0).ToList();// 5  elementos (new)    
+    var numerosParesMayores5 = numerosPares.Where(x=>x>5).ToList();// 3 elementos (new)
+
+
+    //Segundo escenario
+    var queryPares = numeros.Where(x => x % 2 == 0);
+    var queryParesMayores5 = queryPares.Where(x => x > 5);
+
+    var _numerosParesMayores5 = queryParesMayores5.ToList();//3 elemmentos  (new)
+
+    //Tercer escenario
+    var __numerosParesMayores5 = numeros.Where(x => x %2==0 &&  x>5).ToList();
+
+}
+
+void BuscarPersona(List<Persona> personas, string nombre, string apellido, string ciudad)
+{
+
+    var query = personas.Select(x=>x);
+
+    if (!string.IsNullOrEmpty(nombre))
+        query = query.Where(x => x.Nombres.Contains(nombre));
+
+    if (!string.IsNullOrEmpty(apellido))
+        query= query.Where(x=>x.Apellidos.Contains(apellido));
+
+    if (!string.IsNullOrEmpty(ciudad))
+        query = query.Where(x => x.Ciudad.Contains(ciudad));
+
+
+    //foreach (var item in query.ToList())
+    //{
+    //    Console.WriteLine(item.Nombres);
+    //}
+    Console.WriteLine("Búsqueda Terminada");
+
+
+    //Alternativa B (Ineficiente)
+    //if (!string.IsNullOrEmpty(nombre))
+    //{
+    //    var personasPorNombre = personas.Where(x => x.Nombres.Contains(nombre)).ToList();
+
+    //    if (!string.IsNullOrEmpty(apellido))
+    //    {
+    //        var personasPorApellido = personasPorNombre.Where(x=>x.Apellidos.Contains(apellido)).ToList();
+
+    //        if (!string.IsNullOrEmpty(ciudad))
+    //        {
+    //            var personasPorCiudad = personasPorApellido.Where(x => x.Ciudad.Contains(ciudad)).ToList();
+
+    //        }
+
+    //    }
+    //}
+
+
+
+    //Alternativa C (Confusa)
+    //var encontrados = personas.Where(x => x.Nombres.Contains(nombre)
+    //                                 && x.Apellidos.Contains(apellido)
+    //                                 && x.Ciudad.Contains(ciudad)
+    //                                ).ToList();
+
+
+
+
+}
+
+//BuscarPersona(PersonaData.GetPersonas(), "1", "", "");//Solo por nombres
+//BuscarPersona(PersonaData.GetPersonas(), "", "2", "");// Solo por apellidos
+//BuscarPersona(PersonaData.GetPersonas(), "", "", "3");// Solo por ciudad
+//BuscarPersona(PersonaData.GetPersonas(), "1", "1", "1");//Por los 3
+BuscarPersona(PersonaData.GetPersonas(), "1", "2", "");// por nombres y apellido
+
+
+
+
+
+
+//MostrarNumerosParesLINQ();
+//MostrarNumerosParesLambda();
+
+Console.Read();
+
+
